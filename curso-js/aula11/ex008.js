eefd6a6c9604e725dba6f29275370fa7f584fd20
Carlos Eduardo Sisso <carloseduardossz@gmail.com>
2024-10-14 16:04:17 -0300

    Exercício 008, 009 e 010
    Aula 11 – Condições (Parte 1) - Curso de Javascript - Curso em Vídeo

---
+++ curso-js/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+
+if(vel > 60) { // Esta é uma condição simples.
+    console.log('VOCÊ FOI MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
