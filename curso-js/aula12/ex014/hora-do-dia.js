18d21566e82c1be83875b5cfef41540bf451e6d2
Carlos Eduardo Sisso <carloseduardossz@gmail.com>
2024-10-15 13:32:48 -0300

    Atualizei o arquivo "Hora do Dia"

--- curso-js/aula12/ex014/hora-do-dia.js
+++ curso-js/aula12/ex014/hora-do-dia.js
@@ -3,7 +3,9 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var minutos = data.getMinutes()
+    var hhmm = [hora, minutos].join('h')
+    msg.innerHTML = `Agora são ${hhmm}.`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
         msg.innerHTML += '<p><strong>Bom dia!</strong></p>'
