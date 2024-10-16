f92c7f76a9e3891362a9443cec4bea57d321968a
Carlos Eduardo Sisso <carloseduardossz@gmail.com>
2024-10-16 13:00:58 -0300

    Iniciando compreensão de estruturas de repetição
    Aula 13 – Repetições (Parte 1) - Curso de JavaScript - Curso em Vídeo

---
+++ curso-js/aula13/ambiente.js
@@ -0,0 +1,21 @@
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NA FORÇA BRUTA (HAHAHA!)
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+ 
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+var c = 1
+while (c <= 6) {
+    console.log(`Tudo bem nº ${c}`)
+    c++
+}
+
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
+var c = 1
+do {
+    console.log(`Tudo bem nº ${c}`)
+    c++
+} while (c <= 6)
