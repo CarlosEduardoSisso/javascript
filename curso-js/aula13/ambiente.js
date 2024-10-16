283e334b0d4c6f58531fe5ba556447064b9fb019
Carlos Eduardo Sisso <carloseduardossz@gmail.com>
2024-10-16 13:40:48 -0300

    Aula 14 – Repetições (Parte 2)
    Aula 14 – Repetições (Parte 2) - Curso de JavaScript - Curso em Vídeo

--- curso-js/aula13/ambiente.js
+++ curso-js/aula13/ambiente.js
@@ -1,20 +1,27 @@
-// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NA FORÇA BRUTA (HAHAHA!)
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NA FORÇA BRUTA (HAHAHA!) -- SEM LÓGICA NENHUMA NA VERDADE.
+console.log('Sem Lógica nenhuma nº 1')
+console.log('Sem Lógica nenhuma nº 2')
+console.log('Sem Lógica nenhuma nº 3')
+console.log('Sem Lógica nenhuma nº 4')
+console.log('Sem Lógica nenhuma nº 5')
+console.log('Sem Lógica nenhuma nº 6')
+
 
  // ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
 var c = 1
 while (c <= 6) {
-    console.log(`Tudo bem nº ${c}`)
+    console.log(`Teste lógico no início nº ${c}`)
     c++
 }
 
 // ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
 var c = 1
 do {
-    console.log(`Tudo bem nº ${c}`)
+    console.log(`Teste lógico no final nº ${c}`)
     c++
 } while (c <= 6)
+
+// ESTRUTURA DE REPETIÇÃO COM VARIÁVEL DE CONTROLE
+for (var c = 1;c <= 6;c++) {
+    console.log(`Teste lógico com variável de controle nº ${c}`)
+}
